aa04c99058dc99290481e680e77b2ad50b5c2240
Jece Xavier <Jece Xavier@Brainiac.local>
2019-07-13 20:02:33 -0300

    Correcao de Bugs

---
+++ TesteTecnicoJL/src/main/java/testejl/testetecnicojl/Modelo/DAO/ConnectionHibernate.java
@@ -0,0 +1,29 @@
+package testejl.testetecnicojl.Modelo.DAO;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class ConnectionHibernate {
+    private static EntityManagerFactory factory;
+    private static EntityManager manager;
+    
+    private ConnectionHibernate(){ }
+    
+    public static EntityManager getInstance(){
+        if (manager == null){
+            synchronized (ConnectionHibernate.class){
+                if (manager == null){
+                factory = Persistence.createEntityManagerFactory("TesteJL_TesteTecnicoJL_jar_1.0-SNAPSHOTPU");
+                manager = factory.createEntityManager();
+                }
+            }
+        }
+        return manager;
+    }
+    
+    public static void close(){
+        manager.close();
+        factory.close();
+    }
+}
